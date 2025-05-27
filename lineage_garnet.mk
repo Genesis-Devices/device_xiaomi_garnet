c88efd3f35ba9d5ab49e60ae5de783a4f7402b26
baka-hokage <ayushbehera007@gmail.com>
2025-05-27 19:04:52 +0000

    garnet: Init Genesis
    Signed-off-by: baka-hokage <ayushbehera007@gmail.com>

--- lineage_garnet.mk
+++ lineage_garnet.mk
@@ -10,6 +10,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+GENESIS_MAINTAINER := Ayush
+GENESIS_OFFICIAL := true
 
 # Inherit from garnet device
 $(call inherit-product, device/xiaomi/garnet/device.mk)
